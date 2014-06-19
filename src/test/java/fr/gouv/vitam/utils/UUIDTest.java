6a47a0af41971131611d4858dd27baa454e72fa7
fbregier <fbregier@scm-manager.local>
2014-06-19 17:49:04 +0200

    Fix Junit UUID long time

--- src/test/java/fr/gouv/vitam/utils/UUIDTest.java
+++ src/test/java/fr/gouv/vitam/utils/UUIDTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
 
 public class UUIDTest {
 	private static final char VERSION           = 'd';
-	private static int NB = 1000000;
+	private static int NB = 100000;
 	
     @Test
     public void testStructure() {
